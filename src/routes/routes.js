0aee4d148b42c9875691bda4c1b0dff082f5dbc2
Nico Ledesma <nicoledesma36@gmail.com>
2018-07-12 19:34:56 -0300

    token inicial funcionando

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const routes = express.Router();
+const jwt = require('jsonwebtoken');
 
-routes.get('/', (req, res)=>{
+const passtoken ='LaPassSecret';
+
+routes.get('/', (req, res) => {
     res.render('index');
 });
 
-module.exports=routes;
+routes.post('/login', (req, res) => {
+    let {username, password} = req.body;
+    if (username != 'nico' && password != 'asd') res.send('ña ña ña ña');
+
+    let data = {
+        username: username,
+        apellido: "Ledesma",
+        nombre: "Nicolás Maximiliano",
+        dni: 36025898
+    }
+    let token = jwt.sign(data, passtoken, {
+        expiresIn: 60 // en segundos
+    });
+    res.send(token);
+});
+
+routes.get('/admin', (req,res)=>{
+   let token = req.headers['authorization'];
+   if(!token)
+       res.status(401).send( { error: "Necesario el token para ingresar"  } );
+   console.log(token);
+   token=token.replace('Bearer','');
+
+   jwt.verify(token, passtoken, (err, user)=>{
+       (err) ?
+           res.status(401).send({ error: "Token inválido." }) :
+           res.send({ mensaje: 'good' });
+
+   });
+
+});
+
+module.exports = routes;
